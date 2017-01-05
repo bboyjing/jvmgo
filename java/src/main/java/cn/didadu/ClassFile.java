63d76d35ae1047e8d96fbd6daed86cdb89915653
张靖 <zhangjing2@aixuedai.com>
2017-01-05 10:32:02 +0800

    add java project

---
+++ java/src/main/java/cn/didadu/ClassFile.java
@@ -0,0 +1,19 @@
+package cn.didadu;
+
+/**
+ * Created by zhangjing on 17-1-1.
+ */
+public class ClassFile {
+    public static final boolean FLAG = true;
+    public static final byte BYTE = 123;
+    public static final char X = 'X';
+    public static final short SHORT = 12345;
+    public static final int INT = 123456789;
+    public static final long LONG = 12345678901L;
+    public static final float PI = 3.14f;
+    public static final double E = 2.71828;
+
+    public static void main(String[] args) throws RuntimeException {
+        System.out.println("Hello, World!");
+    }
+}
