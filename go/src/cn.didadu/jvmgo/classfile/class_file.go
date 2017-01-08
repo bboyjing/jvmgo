18cdb1e7ab37583593221054c8c452b11940b51c
张靖 <zhangjing2@aixuedai.com>
2017-01-08 19:38:14 +0800

    update member_info.go && add java1.8 compiler

--- go/src/cn.didadu/jvmgo/classfile/class_file.go
+++ go/src/cn.didadu/jvmgo/classfile/class_file.go
@@ -34,3 +34,52 @@ type ClassFile struct {
 	methods      []*MemberInfo
 	attributes   []AttributeInfo
 }
+
+// 读取魔数
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader)  {
+	magic := reader.readUint32()
+	if magic != 0xCAFEBABE {
+		/*
+			Java虚拟机的实现是抛出java.lang.ClassFormatError
+			目前由于才开始写虚拟机，还没法抛出异常，暂先调用panic
+		 */
+		panic("java.lang.ClassFormatError: magic!")
+	}
+}
+
+// 读取版本号
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	switch  self.majorVersion {
+	// 主版本号为45直接返回
+	case 45:
+		return
+	// 主版本号为46~52时，次版本号必须为0
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
+}
+
+// 读取类访问标志
+func (self *ClassFile) readAccessFlags(reader *ClassReader) {
+	self.accessFlags = reader.readUint16()
+}
+
+// 读取类和超类索引
+func (self *ClassFile) readThisClass(reader *ClassReader) {
+	self.thisClass = reader.readUint16()
+}
+
+func (self *ClassFile) readSuperClass(reader *ClassReader) {
+	self.superClass = reader.readUint16()
+}
+
+// 读取接口索引表
+func (self *ClassFile) readInterface(reader *ClassReader) {
+	//接口是u2类型的表结构，所以用的readUint16s方法读取
+	self.interfaces = reader.readUint16s()
+}
--- go/src/cn.didadu/jvmgo/classfile/member_info.go
+++ go/src/cn.didadu/jvmgo/classfile/member_info.go
@@ -24,3 +24,27 @@ type MemberInfo struct {
 	descriptorIndex uint16
 	attributes      []AttributeInfo
 }
+
+// 读取字段表或者方法表
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo {
+	// 获取fields_count或者methods_count
+	memberCount := reader.readUint16()
+	members := make([]*MemberInfo, memberCount)
+	// 读取每一个field或者method
+	for i := range members {
+		members[i] = readMember(reader, cp)
+	}
+	return members
+}
+
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
+	// 读取class文件中对应的字节
+	return &MemberInfo{
+		cp:              cp,
+		accessFlags:     reader.readUint16(),
+		nameIndex:       reader.readUint16(),
+		descriptorIndex: reader.readUint16(),
+		// 读取属性表后面再讲
+		//attributes:      readAttributes(reader, cp),
+	}
+}
